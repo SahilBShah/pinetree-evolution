2deff297cd6f55fad40acaf463207035aef1ac01
Sahil Shah <sahil2699@gmail.com>
2019-10-22 15:58:47 -0500

    Grant data for TACC

--- src/python/random_data_generator.py
+++ src/python/random_data_generator.py
@@ -9,23 +9,26 @@ proteinZ_list = []
 
 for i in range(1, 81):
     transcript_list.append([i, "proteinX", (0.3 * i)])
-    #if i == 150:
-    #    proteinX_list = transcript_list[-1]
-    transcript_list.append([i, "proteinY", (0.5 * i)])
+    if i == 80:
+        proteinX_list = transcript_list[-1]
+    transcript_list.append([i, "proteinY", (0.4 * i)])
     if i == 80:
         proteinY_list = transcript_list[-1]
-    transcript_list.append([i, "proteinZ", (0.1 * i)])
+    transcript_list.append([i, "proteinZ", (0.2 * i)])
     if i == 80:
         proteinZ_list = transcript_list[-1]
-for i in range(81, 241):
-    transcript_list.append([i, "proteinX", (0.3 * i)])
+for i in range(81, 151):
+    transcript_list.append([i, "proteinX", proteinX_list[-1]])
     transcript_list.append([i, "proteinY", proteinY_list[-1]])
     transcript_list.append([i, "proteinZ", proteinZ_list[-1]])
+for i in range(151, 241):
+    transcript_list.append([i, "proteinX", proteinX_list[-1]])
+    transcript_list.append([i, "proteinY", (-0.15 * i) + (0.4 * 80) + (0.15 * 151)])
+    transcript_list.append([i, "proteinZ", proteinZ_list[-1]])
 
-transcript_dataframe = pandas.DataFrame()
-export_csv = transcript_dataframe.to_csv("~/pinetree-toys/three_genes_evolution/needed_evolution_files/random_data4.tsv", index=False)
 
-file = open("random_data5.tsv", "w")
+
+file = open("../../data/grant_data/grant_data10.tsv", "w")
 writer = csv.writer(file, delimiter='\t')
 writer.writerow(["time", "species", "transcript"])
 for item in transcript_list:
