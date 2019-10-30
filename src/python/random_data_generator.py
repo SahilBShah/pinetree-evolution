c597d85ee03bfbac700f37d010ab6dc3b6f05af8
Sahil Shah <sahil2699@gmail.com>
2019-10-30 15:48:54 -0500

    Remaking grant data 1 graph

--- src/python/random_data_generator.py
+++ src/python/random_data_generator.py
@@ -7,24 +7,24 @@ proteinX_list = []
 proteinY_list = []
 proteinZ_list = []
 
-for i in range(1, 151):
-    transcript_list.append([i, "proteinX", (0.1 * i)])
+for i in range(1, 251):
+    transcript_list.append([i, "proteinX", (0.2 * i)])
     if i == 150:
         proteinX_list = transcript_list[-1]
-    transcript_list.append([i, "proteinY", (0.2 * i)])
+    transcript_list.append([i, "proteinY", (0.15 * i)])
     if i == 150:
         proteinY_list = transcript_list[-1]
-    transcript_list.append([i, "proteinZ", (0.15 * i)])
+    transcript_list.append([i, "proteinZ", (0.1 * i)])
     if i == 150:
         proteinZ_list = transcript_list[-1]
-for i in range(151, 251):
-    transcript_list.append([i, "proteinX", (0.1 * i)])
-    transcript_list.append([i, "proteinY", proteinY_list[-1]])
-    transcript_list.append([i, "proteinZ", (-0.05 * i) + (0.15 * 200)])
+# for i in range(151, 251):
+#     transcript_list.append([i, "proteinX", (0.1 * i)])
+#     transcript_list.append([i, "proteinY", proteinY_list[-1]])
+#     transcript_list.append([i, "proteinZ", (-0.05 * i) + (0.15 * 200)])
 
 
 
-file = open("../../data/grant_data/grant_data13.tsv", "w")
+file = open("../../data/grant_data/grant_data1.tsv", "w")
 writer = csv.writer(file, delimiter='\t')
 writer.writerow(["time", "species", "transcript"])
 for item in transcript_list:
