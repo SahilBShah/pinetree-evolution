04cbba606c8e3f37e3ef7b6532aed9d5e405362b
Sahil Shah <sahil2699@gmail.com>
2020-04-20 12:52:22 -0500

    Added ability to save initial template genome data and updated data analysis files

--- src/python/random_data_generator.py
+++ src/python/random_data_generator.py
@@ -9,15 +9,15 @@ protein3_list = []
 
 for i in range(1, 251):
     transcript_list.append([i, "protein1", (0.2 * i)])
-    transcript_list.append([i, "protein2", (0.18 * i)])
-    transcript_list.append([i, "protein3", (0.16 * i)])
-    transcript_list.append([i, "protein4", (0.14 * i)])
-    transcript_list.append([i, "protein5", (0.12 * i)])
-    transcript_list.append([i, "protein6", (0.10 * i)])
-    transcript_list.append([i, "protein7", (0.08 * i)])
-    transcript_list.append([i, "protein8", (0.06 * i)])
-    transcript_list.append([i, "protein9", (0.04 * i)])
-    transcript_list.append([i, "protein10", (0.02 * i)])
+    transcript_list.append([i, "protein2", (0.12 * i)])
+    transcript_list.append([i, "protein3", (0.04 * i)])
+    # transcript_list.append([i, "protein4", (0.14 * i)])
+    # transcript_list.append([i, "protein5", (0.12 * i)])
+    # transcript_list.append([i, "protein6", (0.10 * i)])
+    # transcript_list.append([i, "protein7", (0.08 * i)])
+    # transcript_list.append([i, "protein8", (0.06 * i)])
+    # transcript_list.append([i, "protein9", (0.04 * i)])
+    # transcript_list.append([i, "protein10", (0.02 * i)])
 # for i in range(71, 101):
 #     transcript_list.append([i, "protein1", protein1_list[-1]])
 # for i in range(101, 251):
@@ -31,7 +31,7 @@ for i in range(1, 251):
 
 
 
-file = open("../../data/grant_data18.tsv", "w")
+file = open("../../data/grant_data19.tsv", "w")
 writer = csv.writer(file, delimiter='\t')
 writer.writerow(["time", "species", "transcript"])
 for item in transcript_list:
