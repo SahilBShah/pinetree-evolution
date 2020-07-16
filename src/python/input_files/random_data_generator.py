846d8d13a92bc38731fb928177c4c8adcb59d4c1
Sahil Shah <sahil2699@gmail.com>
2020-07-16 11:09:28 -0500

    Added more target files.

--- src/python/input_files/random_data_generator.py
+++ src/python/input_files/random_data_generator.py
@@ -9,23 +9,23 @@ protein3_list = []
 
 for i in range(1, 101):
     transcript_list.append([i, "protein1", (0.20 * i)])
-    transcript_list.append([i, "protein2", (0.10 * i)])
-    transcript_list.append([i, "protein3", (0.05 * i)])
+    transcript_list.append([i, "protein2", (0.17 * i)])
+    transcript_list.append([i, "protein3", (0.08 * i)])
     if i == 100:
         protein1_list.append(0.20 * i)
-        protein2_list.append(0.10 * i)
-        protein3_list.append(0.05 * i)
+        protein2_list.append(0.17 * i)
+        protein3_list.append(0.08 * i)
 for i in range(101, 301):
-    transcript_list.append([i, "protein1", protein1_list[-1]])
-    transcript_list.append([i, "protein2", (0.10 * i)])
-    transcript_list.append([i, "protein3", (0.05 * i)])
+    transcript_list.append([i, "protein1", (0.20 * i)])
+    transcript_list.append([i, "protein2", protein2_list[-1]])
+    transcript_list.append([i, "protein3", (0.08 * i)])
 # for i in range(226, 301):
 #     transcript_list.append([i, "protein1", (-0.05 * i + 26.3)])
 #     transcript_list.append([i, "protein2", protein2_list[-1]])
 #     transcript_list.append([i, "protein3", (0.05 * i)])
 
 
-file = open("../../../data/targets/paper_data17.tsv", "w")
+file = open("../../../data/targets/paper_data20.tsv", "w")
 writer = csv.writer(file, delimiter='\t')
 writer.writerow(["time", "species", "transcript"])
 for item in transcript_list:
