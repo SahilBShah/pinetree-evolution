a9d61b5be9b12502ad705470745d929f821abda7
Sahil Shah <sahil2699@gmail.com>
2020-08-10 19:49:01 -0500

    Changed simulation generations to 5000 in bash script.

--- src/python/evolution_execute.sh
+++ src/python/evolution_execute.sh
@@ -11,11 +11,11 @@
 for i in $(seq 1 66; i+=2); do
     #Then iterate through the hundreds (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     for j in $(seq 1 20); do
-        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$j '10000 10 True &' 
+        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$j '5000 10 True &' 
     done
     i=$(($i+1))
     for k in $(seq 1 20); do
-        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$k '10000 10 True &'
+        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$k '5000 10 True &'
     done
     wait #WAIT. This is super important, make sure all those eval calls finish before moving on
         
