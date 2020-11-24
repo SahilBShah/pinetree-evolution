f9a3e27f12f687ae21505edd17682bb1930e6942
Sahil Shah <sahil2699@gmail.com>
2020-11-24 13:13:55 -0600

    Removed misc files.

--- src/python/manuscript_figures_help/evolution_rsync.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system("rsync -av -e ssh --include=\"expression_pattern_best.tsv\" --include=\"gene_best.yml\" --include=\"rmse_data.tsv\" \
-	--include=\"/paper_data*\" --include=\"/final*\" --exclude=\"expression_pattern_*\" --exclude=\"gene_*\" --exclude=\"config.yml\" \
-	sbs2756@wilkcomp02.ccbb.utexas.edu:/stor/work/Wilke/sshah/pinetree-toys/results/ /home/sahil/pinetree-toys/results/")
