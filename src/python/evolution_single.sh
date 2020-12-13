93ee955b7c1369b98bdbc9e5134a3c8eb30a6ece
Sahil Shah <sahil2699@gmail.com>
2020-12-13 14:02:57 -0600

    Altered directory structure and editing figure scripts.

--- src/python/evolution_single.sh
+++ src/python/evolution_single.sh
@@ -1,11 +1,11 @@
 #!/bin/bash                                                                                                                                                                                                        
 
 for j in $(seq 1 10); do
-    eval 'nohup python evolution.py paper_data13.tsv testing.yml' \$j '5000 10 &'
-    eval 'nohup python evolution.py paper_data64.tsv testing.yml' \$j '5000 10 &'
-    eval 'nohup python evolution.py paper_data67.tsv testing.yml' \$j '5000 10 &'
-    eval 'nohup python evolution.py paper_data79.tsv testing.yml' \$j '5000 10 &'
-    eval 'nohup python evolution.py paper_data127.tsv testing.yml' \$j '5000 10 &'
+    eval 'nohup python evolution.py paper_data1_arrange1.tsv testing.yml' \$j '5000 10 &'
+    eval 'nohup python evolution.py paper_data2_arrange2.tsv testing.yml' \$j '5000 10 &'
+    eval 'nohup python evolution.py paper_data3_arrange3.tsv testing.yml' \$j '5000 10 &'
+    eval 'nohup python evolution.py paper_data4_arrange4.tsv testing.yml' \$j '5000 10 &'
+    eval 'nohup python evolution.py paper_data5_arrange5.tsv testing.yml' \$j '5000 10 &'
 done
 wait #WAIT. This is super important, make sure all those eval calls finish before moving on
 
