66b210a6cbabc858fa7c63fc9212a17008ca50d8
Sahil Shah <sahil2699@gmail.com>
2020-12-13 14:02:57 -0600

    Altered directory structure and editing figure scripts.

--- src/python/evolution_execute.sh
+++ src/python/evolution_execute.sh
@@ -1,14 +1,13 @@
 #!/bin/bash                                                                                                                                                                                                        
 
-#This loop will run 66 simulations in batches of 25
-for i in $(seq 1 2 66); do
+#This loop will run 60 simulations in batches of 50
+for i in $(seq 1 2 10); do
     #Iterate through batches of 25
-    for j in $(seq 1 25); do
-        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$j '5000 10 &' 
+    for j in $(seq 1 6); do
+        for k in $seq(1 50); do
+            eval 'nohup python evolution.py paper_data'\$i'_arrange'\$j'.tsv testing.yml' \$k '5000 10 &' 
+        done    
+        wait  
     done
-    i=$(($i+1))
-    for k in $(seq 1 25); do
-        eval 'nohup python evolution.py paper_data'\$i'.tsv testing.yml' \$k '5000 10 &'
-    done
-    wait #WAIT. This is super important, make sure all those eval calls finish before moving on
+    wait
 done
