ae1479bdad264aecfd6b08d51ce67a43cdfe9d68
SahilBShah <sahil2699@gmail.com>
2021-03-06 21:49:03 -0600

    Moved bash scripts and manuscript folder from this branch.

--- src/bash/evolution_execute.sh
+++ src/bash/evolution_execute.sh
@@ -1,6 +1,6 @@
-#!/bin/bash                                                                                                                                                                                                        
+#!/bin/bash
 
-#This loop will run 60 simulations in batches of 50
+#This loop will run 3000 simulations in total. This will be done in batches of 50 and split up by each expression pattern file.
 for i in $(seq 1 2 10); do
     #Iterate through batches of 25
     for j in $(seq 1 6); do
